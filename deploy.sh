8f29f6141e78388aa094efb6014e59dde3c4cd74
Robbe Louwet <robbe.louwet@nara.to>
2023-11-19 11:31:23 +0100

    init commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+RG=rg-robbelouwet-02
+PARAMETERS_FILE=parameters.json
+
+az deployment group create --resource-group $RG --template-file ./infra.bicep --parameters $PARAMETERS_FILE
+
+az deployment group create --resource-group $RG --template-file ./main.bicep --parameters $PARAMETERS_FILE
