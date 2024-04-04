f61a9f792755be2e5e6c98be973bccc737493aec
Robbe Louwet <robbe.louwet@nara.to>
2024-04-04 09:42:41 +0200

    added automatic RAM allocation

--- paper-consumption/entrypoint.sh
+++ paper-consumption/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "JVM_ARGS: ${JVM_ARGS}"
+ram=$(free --mega | grep "Mem" | awk '{print $2}')
+
+# trick to do rounded floating point arithmetic in bash
+minRAM=$(printf %.f $(echo $ram*0.05 | bc))
+maxRAM=$(printf %.f $(echo $ram*0.95 | bc))
+
+echo "available/min/max RAM (MB): ${ram} / ${minRAM} (5%) / ${maxRAM} (95%)"
 
 # Set up server files if we're running for the first time
 if [ ! -d "/data" ] || [ -z "$(ls -A /data)" ]; then
@@ -21,4 +27,4 @@ yq -i '.proxies.velocity.secret = strenv(VELOCITY_SECRET)' /data/config/paper-gl
 rm -rf world*/session.lock
 
 # Run the server
-python3 /process_stdio_wrapper.py java -jar $JVM_ARGS /*.jar --nogui
+python3 /process_stdio_wrapper.py java -jar -Xms${minRAM}M -Xmx${maxRAM}M /*.jar --nogui
